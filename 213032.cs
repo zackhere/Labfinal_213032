6888debf0113041d1528657a91615448f7db2697
zackhere <155349924+zackhere@users.noreply.github.com>
2024-01-01 12:20:46 +0500

    Add files via upload

---
+++ 213032.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+public class ReceptionistController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ReceptionistController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Appointments()
+    {
+        var appointments = _context.Appointments.Include(a => a.Patient).Include(a => a.Doctor).ToList();
+        return View(appointments);
+    }
+
+}
